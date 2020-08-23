8abee0d2b83a9694db2c1e80dd91f7a6ad0e59db
Victor <victor1kohut@gmail.com>
2020-08-23 22:31:17 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/*jslint es6:true*/
+/*jshint esversion: 6 */
+
+
+"use strict";
+
+// alert('hello victor');
+// alert(1);
+
+// const result = confirm('are you here?');
+// console.log(result);
+
+// const result = prompt("сколько вам лет?", "");
+// console.log(typeof(result));
+
+// const user = 'Ivan';
+// alert(`Привет, ${user}`);
+
+// const category = 'toys';
+// console.log(`httml://someural.com/${category}`);
